9231380ecd985a0bf24d3ed57ad2253337e8361b
JustLon <lgabrielmonzon@hotmail.com>
2024-05-21 20:06:59 -0600

    Hot fixes and more

--- AplicacionFinal.py
+++ AplicacionFinal.py
@@ -3,12 +3,17 @@ from collections import deque
 import numpy as np
 import random
 import pygame
+from colorama import init, Fore, Style
+from art import *
+from pprint import pformat
+
+init()
 
 # Definir movimientos posibles (arriba, abajo, izquierda, derecha)
 moves = [(0, 1), (0, -1), (1, 0), (-1, 0)]
 
 def is_valid_move(maze, row, col):
-    # Verificar si la celda está dentro de los límites del laberinto y si es un pasillo
+    # Verificar si la celda esta dentro de los limites del laberinto y si es un pasillo
     return 0 <= row < len(maze) and 0 <= col < len(maze[0]) and maze[row][col] == 0
 
 def bfs(maze, start, end, canvas):
@@ -36,7 +41,7 @@ def bfs(maze, start, end, canvas):
         canvas.update()
         canvas.after(100)
 
-    return -1, None  # No se encontró ruta hasta la celda de salida
+    return -1, None  # No se encontro ruta hasta la celda de salida
 
 def reconstruct_path(parents, start, end):
     # Reconstruir la ruta desde la celda de inicio hasta la celda de salida
@@ -52,7 +57,7 @@ def find_shortest_path(maze, start, end, canvas):
     # Ejecutar BFS para encontrar la distancia más corta desde la celda de inicio hasta la celda de salida
     distance, parents = bfs(maze, start, end, canvas)
     if distance == -1:
-        return None, -1  # No se encontró una ruta válida
+        return None, -1  # No se encontro una ruta válida
     else:
         return reconstruct_path(parents, start, end), distance
 
@@ -85,11 +90,13 @@ def start_bfs():
     canvas.create_rectangle(start[1]*cell_size, start[0]*cell_size, (start[1]+1)*cell_size, (start[0]+1)*cell_size, fill='yellow')
     canvas.create_rectangle(end[1]*cell_size, end[0]*cell_size, (end[1]+1)*cell_size, (end[0]+1)*cell_size, fill='red')
 
-    # Encontrar la ruta más corta
+    # Encontrar la ruta mas corta
     path, distance = find_shortest_path(maze, start, end, canvas)
     if path is not None:
-        print("Ruta más corta:", path)
-        print("Distancia:", distance)
+        pathPretty = pformat([path], indent=4)
+        distancePretty = pformat(distance, indent=4)
+        print(f"{Fore.GREEN}{Style.BRIGHT}Ruta más corta:\n{Style.RESET_ALL}  {Fore.CYAN}{Style.BRIGHT}{pathPretty}{Style.RESET_ALL}")
+        print(f"{Fore.YELLOW}{Style.BRIGHT}Distancia:{Style.RESET_ALL} {Fore.CYAN}{Style.BRIGHT}{distancePretty}{Style.RESET_ALL}")
         # Marcar el camino óptimo
         for row, col in path[1:-1]:
             canvas.create_rectangle(col*cell_size, row*cell_size, (col+1)*cell_size, (row+1)*cell_size, fill='darkgreen')
@@ -101,7 +108,7 @@ def start_bfs():
         pygame.mixer.music.set_volume(0.5)
         pygame.mixer.music.play()
 
-        root.after(5000, start_bfs)
+        root.after(10000, start_bfs)
     else:
         for row in range(len(maze)):
             for col in range(len(maze[0])):
@@ -113,7 +120,7 @@ def start_bfs():
         pygame.mixer.music.set_volume(0.5)
         pygame.mixer.music.play()
 
-        root.after(2000, start_bfs)
+        root.after(5000, start_bfs)
 
 # Configuración de la ventana
 rows, cols = 15, 15
@@ -125,7 +132,7 @@ maze = generate_maze(rows, cols, p=0.7)
 # Crear ventana y lienzo
 root = tk.Tk()
 root.title("Laberinto con BFS")
-root.geometry('{}x{}'.format((cols+1)*cell_size, (rows+2)*cell_size))  # Ajustar tamaño de ventana
+root.geometry('{}x{}'.format((cols+1)*cell_size, (rows+2)*cell_size))  # Ajustar size de ventana
 root.resizable(False, False)  # Impedir que la ventana se pueda redimensionar
 canvas = tk.Canvas(root, width=cols*cell_size, height=rows*cell_size)
 canvas.pack()
@@ -138,6 +145,7 @@ exit_button.pack()
 start_bfs_button = tk.Button(root, text="Iniciar BFS", command=start_bfs)
 start_bfs_button.pack()
 
+tprint("AgentesPy", "rnd-xlarge")
+
 start_bfs()
 root.mainloop()
-
